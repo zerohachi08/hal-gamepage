cdf77cec0f795b3dd299b9caf7d1a5e0a7fd9584
zerohachi08 <zerohachi.github@gmail.com>
2024-03-19 23:48:45 +0900

    up

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+function csv2json(csvArray){
+  let jsonArray = [];
+
+  let RowArray = csvArray.split('\n');
+  let items = RowArray[0].split(',');
+  for(let i = 1; i < RowArray.length; i++){
+      let cellArray = RowArray[i].split(',');
+      let line = new Object();
+      for(let j = 0; j < items.length; j++){
+          line[items[j]] = cellArray[j];
+      }
+      jsonArray.push(line);
+  }
+  return jsonArray;
+}
+
 let path = './sheet1.csv';
-const promise = () => new Promise((resolve, reject) => {
-  fs.readFile(path, 'utf-8', (err, data) => {
-    resolve(data);
+const promise = () => fetch("sheet1.csv")
+.then(function(response){
+   if (!response.ok) {return Promise.reject(new Error("error"));}
+   return response.text();
+}) 
+.then(function(text){
+  const jsondata = csv2json(text);
+  
+  jsondata.forEach((element) => {
+    document.querySelector("#include").insertAdjacentHTML('afterbegin', '<div class="student"></div>');
+    document.querySelector(".student").insertAdjacentHTML('beforeend', '<p>クラス名:' + element['class'] + '</p>');
+    document.querySelector(".student").insertAdjacentHTML('beforeend', '<p>出席番号:' + element['number'] + '</p>');
+    document.querySelector(".student").insertAdjacentHTML('beforeend', '<p>名前:' + element['name'] + '</p>');
+    document.querySelector(".student").insertAdjacentHTML('beforeend', '<p>ゲーム名:' + element['gametitle'] + '</p>');
+    document.querySelector(".student").insertAdjacentHTML('beforeend', '<p>画像名:' + element['gameimage'] + '</p>');
   });
-})
-.then((data) => {
-  console.log(data);
-  console.log("resolveしたよ");
-})
-.catch((err) => {
-  console.log(err);
-  console.log("rejectしたよ");
+}) 
+.catch(function(error){console.error("fetch error", error);  //  error処理
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
