6c9a332daeee6469c13f2f79b5f0736b02da6d44
zerohachi08 <zerohachi.github@gmail.com>
2024-03-19 23:14:19 +0900

    update: index.htmlとscript.jsの内容を修正
    面倒だから、json読み込みに変更

---
+++ script.js
@@ -0,0 +1,14 @@
+let path = './sheet1.csv';
+const promise = new Promise((resolve, reject) => {
+  reject();
+})
+.then(() => {
+  console.log("resolveしたよ");
+})
+.catch(() => {
+  console.log("rejectしたよ");
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  promise();
+});
